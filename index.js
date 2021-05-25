215f24c38273fa9ce98e771749ae09c5f0040dec
Laura Webber <wwebby1@gmail.com>
2021-05-24 17:10:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function start() {
 
 function paintMap() {
     
-    L.tileLayer('hhttps://api.mapbox.com/v4/mapbox.mapbox-streets-v11/1/0/0.mvt?access_token={accessToken}', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/1/1/0?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
